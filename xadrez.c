262643e373e548a1387a786d960f9c02a21c3add
Caíque Aguiar <165428354+caiqueaguiarg@users.noreply.github.com>
2025-04-15 22:59:02 -0300

    Update xadrez.c
    Codigo intermediario vs code

--- xadrez.c
+++ xadrez.c
@@ -26,6 +26,49 @@ int main() {
         torre++;
     }while(torre < 5);
 
-    return 0;
+
+
+    printf("\nCavalo:\n");
+    int cavalo, movimentos;
+    int direcao1, direcao2;
+
+
+    printf("Primeira opcao de movimentacao: \n");
+    printf("1 - Cima\n2 - Baixo\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &direcao1);
+
+    if(direcao1 != 1 && direcao1 != 2){
+        printf("\n\nOpcao invalida\n\n");
+    } else {
+        printf("\nSegunda opcao de movimentacao: \n");
+        printf("3 - Direita\n4 - Esquerda\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &direcao2);
     
+        if(direcao2 != 3 && direcao2 != 4){
+            printf("\n\nOpcao Invalida\n\n");
+        } else{
+            for(cavalo = 0; cavalo < 1; cavalo++){
+                for(movimentos = 0; movimentos < 2; movimentos++){
+        
+                    if(direcao1 == 1){  
+                        printf("Cima\n");
+                    } else if(direcao1 == 2){ 
+                        printf("Baixo\n");
+                    }
+                }
+        
+                if(direcao2 == 3){
+                    printf("Direita\n");
+                } else if(direcao2 == 4){
+                    printf("Esquerda");
+                }
+            }
+        }
+    
+       
+    }
+   
+    return 0;
 }
